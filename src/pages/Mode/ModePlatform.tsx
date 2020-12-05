99bde91114b65fcb9de63bfb9a504f313f3c6925
Reece Maia <reece.maia@hotmail.com>
2020-12-05 15:29:03 +1000

    bump

--- src/pages/Mode/ModePlatform.tsx
+++ src/pages/Mode/ModePlatform.tsx
@@ -7,7 +7,7 @@ const environment = process.env.REACT_APP_ENVIRONMENT as string;
 export const ModePlatform: FunctionComponent = () => {
   return (
     <div>
-      <div>Platform</div>
+      <div>Platform:</div>
       <div>{`name: ${name}`}</div>
       <div>{`version: ${version}`}</div>
       <div>{`environment: ${environment}`}</div>
